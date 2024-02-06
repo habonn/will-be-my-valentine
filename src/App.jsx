356ff7d4f775605dd744ac5bf138aea229dc3724
Teerut Bunyarat <discuszx7@gmail.com>
2024-02-06 17:25:08 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,60 @@
+import React, { useState } from 'react';
+import { Button } from 'antd';
+
+export function App() {
+  const [showAskPage, setShowAskPage] = useState(true);
+  const [buttonPosition, setButtonStyle] = useState({ left: 0, top: 0 });
+
+  const nextPage = () => {
+    setShowAskPage(false);
+  };
+
+  const moveButton = () => {
+    const x = Math.random() * (window.innerWidth - document.getElementById('noButton').offsetWidth);
+    const y = Math.random() * (window.innerHeight - document.getElementById('noButton').offsetHeight);
+    setButtonStyle({ position: 'absolute', left: `${x}px`, top: `${y}px` });
+  };
+
+  
+
+  return (
+    <div className={`container page`}>
+      {showAskPage && (
+        <div>
+          <h1>Will you go out with me on <span style={{ color: 'red' }}>Valentine’s Day?</span></h1>
+        </div>
+      )}
+      {!showAskPage && (
+        <div>
+          <h1>Look forward to seeing you again.</h1>
+        </div>
+      )}
+      <div className="gif_container">
+        {showAskPage ? (
+          <img src="https://raw.githubusercontent.com/aliArafat9/will-you-go-out-with-me/main/image/ask.gif" alt="Cute animated illustration" />
+        ) : (
+          <img src="https://raw.githubusercontent.com/aliArafat9/will-you-go-out-with-me/main/image/ans.gif" alt="Cute animated illustration X" />
+        )}
+      </div>
+      <div>
+         {showAskPage ? (
+          <Button onClick={nextPage} style={{ marginRight: 10, width: 100, backgroundColor: '#ff3d67', color: 'white' }}>
+            Yes
+          </Button>
+        ) : null}
+        {showAskPage && (
+          <Button
+            id="noButton"
+            style={{ ...buttonPosition, marginLeft: 10,width: 100, backgroundColor: '#ffffff', color: '#ff3d67', cursor: 'pointer', borderColor: '#ff3d67' }}
+            onMouseOver={moveButton}
+            onClick={moveButton}
+          >
+            No
+          </Button>
+        )}
+      </div>
+    </div>
+  );
+}
+
+
